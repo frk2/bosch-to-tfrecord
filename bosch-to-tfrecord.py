1fc9c753820b88a55b8d3cd5eb06f76a2a18d879
Faraz Khan <faraz@slack-corp.com>
2017-10-04 13:27:40 -0700

    Update bosch-to-tfrecord.py

--- bosch-to-tfrecord.py
+++ bosch-to-tfrecord.py
@@ -1,30 +1,3 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-r"""Convert the Oxford pet dataset to TFRecord for object_detection.
-
-See: O. M. Parkhi, A. Vedaldi, A. Zisserman, C. V. Jawahar
-     Cats and Dogs
-     IEEE Conference on Computer Vision and Pattern Recognition, 2012
-     http://www.robots.ox.ac.uk/~vgg/data/pets/
-
-Example usage:
-    ./create_pet_tf_record --data_dir=/home/user/pet \
-        --output_dir=/home/user/pet/output
-"""
-
 import hashlib
 import io
 import logging
@@ -40,14 +13,6 @@ from object_detection.utils import dataset_util
 from object_detection.utils import label_map_util
 import yaml
 from pathlib import Path
-
-
-flags = tf.app.flags
-flags.DEFINE_string('data_dir', '', 'Root directory to raw pet dataset.')
-flags.DEFINE_string('output_dir', '', 'Path to directory to output TFRecords.')
-flags.DEFINE_string('label_map_path', 'data/pet_label_map.pbtxt',
-                    'Path to label map proto')
-FLAGS = flags.FLAGS
 
 def clamp(value):
   return max(min(value, 0.99), 0.01)
