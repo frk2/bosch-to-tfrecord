265a29d124d07ca21fd1dd6ab20c1789780681ea
Faraz Khan <farazrkhan@gmail.com>
2017-10-06 19:17:46 -0700

    - test-bosch script to decipher traffic lights!

--- bosch-to-tfrecord.py
+++ bosch-to-tfrecord.py
@@ -15,7 +15,110 @@ import yaml
 from pathlib import Path
 
 def clamp(value):
-  return max(min(value, 0.99), 0.01)
+  return max(min(value, 1), 0)
+
+def udacity_to_tf_example(img_path, data, force_dir=None):
+  if (force_dir):
+    image = Path(img_path).name
+    img_path = force_dir + '/' + image
+
+  filename = img_path
+  with tf.gfile.GFile(img_path, 'rb') as fid:
+    encoded_jpg = fid.read()
+
+  encoded_image_data = io.BytesIO(encoded_jpg)
+
+  print("Read in {}, {}".format(img_path, data))
+  image_format = b'jpg'
+
+  width = 1920
+  height = 1200
+
+  xmins = []
+  xmaxs = []
+  ymins = []
+  ymaxs = []
+  classes = []
+  classes_text = []
+  for boxes in data:
+    # mark state
+    state = 0
+    if (len(boxes) == 7 and boxes[4] == '0'):
+      state_txt = boxes[6]
+      # if ('Red' in state_txt):
+      #   state_txt = 'Red'
+      #   state = 1
+      # elif ('Yellow' in state_txt):
+      #   state_txt = 'Yellow'
+      #   state = 2
+      # elif ('Green' in state_txt):
+      #   state_txt = 'Green'
+      #   state = 3
+
+      if (state_txt == 'Red'):
+        state = 1
+      elif (state_txt == 'Yellow'):
+        state = 2
+      elif (state_txt == 'Green'):
+        state = 3
+      else:
+        state = -1
+
+      #1-class
+      state_txt = 'light'
+      state = 1
+
+      # elif (state_txt == 'off'):
+      #   state = 4
+      # elif (state_txt == 'GreenStraightRight'):
+      #   state = 5
+      # elif (state_txt == 'GreenStraightLeft'):
+      #   state = 6
+      # elif (state_txt == 'GreenStraight'):
+      #   state = 7
+      # elif (state_txt == 'GreenRight'):
+      #   state = 8
+      # elif (state_txt == 'RedStraightLeft'):
+      #   state = 9
+      # elif (state_txt == 'RedStraight'):
+      #   state = 10
+      # elif (state_txt == 'GreenLeft'):
+      #   state = 11
+      # elif (state_txt == 'RedRight'):
+      #   state = 12
+      # elif (state_txt == 'RedLeft'):
+      #   state = 13
+
+      if (state == 0):
+        raise Exception('state cant be zero bro')
+
+      if (state > 0):
+        xmins.append(clamp(float(boxes[0]) / width))
+        xmaxs.append(clamp(float(boxes[2]) / width))
+        ymins.append(clamp(float(boxes[1]) / height))
+        ymaxs.append(clamp(float(boxes[3]) / height))
+
+        classes_text.append(state_txt)
+        classes.append(state)
+
+  if (len(classes) > 0):
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename.encode('utf8')),
+        'image/source_id': dataset_util.bytes_feature(filename.encode('utf8')),
+        'image/encoded': dataset_util.bytes_feature(encoded_image_data.getvalue()),
+        'image/format': dataset_util.bytes_feature(image_format.encode('utf8')),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+  return None
+
 
 def dict_to_tf_example(data, force_dir=None):
   img_path = data['path']
@@ -48,43 +151,61 @@ def dict_to_tf_example(data, force_dir=None):
     # mark state
     state = 0
     state_txt = boxes['label']
-    if (boxes['label'] == 'Red'):
+    if (boxes['occluded'] == False):
+      # if ('Red' in state_txt):
+      #   state_txt = 'Red'
+      #   state = 1
+      # elif ('Yellow' in state_txt):
+      #   state_txt = 'Yellow'
+      #   state = 2
+      # elif ('Green' in state_txt):
+      #   state_txt = 'Green'
+      #   state = 3
+
+
+      if (boxes['label'] == 'Red'):
+        state = 1
+      elif (boxes['label'] == 'Yellow'):
+        state = 2
+      elif (boxes['label'] == 'Green'):
+        state = 3
+      elif (boxes['label'] == 'off'):
+        state = 4
+      elif (boxes['label'] == 'GreenStraightRight'):
+        state = 5
+      elif (boxes['label'] == 'GreenStraightLeft'):
+        state = 6
+      elif (boxes['label'] == 'GreenStraight'):
+        state = 7
+      elif (boxes['label'] == 'GreenRight'):
+        state = 8
+      elif (boxes['label'] == 'RedStraightLeft'):
+        state = 9
+      elif (boxes['label'] == 'RedStraight'):
+        state = 10
+      elif (boxes['label'] == 'GreenLeft'):
+        state = 11
+      elif (boxes['label'] == 'RedRight'):
+        state = 12
+      elif (boxes['label'] == 'RedLeft'):
+        state = 13
+
+      # 1-class
+      state_txt = 'light'
       state = 1
-    elif (boxes['label'] == 'Yellow'):
-      state = 2
-    elif (boxes['label'] == 'Green'):
-      state = 3
-    elif (boxes['label'] == 'off'):
-      state = 4
-    elif (boxes['label'] == 'GreenStraightRight'):
-      state = 5
-    elif (boxes['label'] == 'GreenStraightLeft'):
-      state = 6
-    elif (boxes['label'] == 'GreenStraight'):
-      state = 7
-    elif (boxes['label'] == 'GreenRight'):
-      state = 8
-    elif (boxes['label'] == 'RedStraightLeft'):
-      state = 9
-    elif (boxes['label'] == 'RedStraight'):
-      state = 10
-    elif (boxes['label'] == 'GreenLeft'):
-      state = 11
-    elif (boxes['label'] == 'RedRight'):
-      state = 12
-    elif (boxes['label'] == 'RedLeft'):
-      state = 13
 
-    if (state == 0):
-      raise Exception('state cant be zero, bro')
+      if (state == 0):
+        raise Exception('state cant be zero, bro')
 
-    xmins.append(clamp(float(boxes['x_min']) / width))
-    xmaxs.append(clamp(float(boxes['x_max']) / width))
-    ymins.append(clamp(float(boxes['y_min']) / height))
-    ymaxs.append(clamp(float(boxes['y_max']) / height))
+      if (state > 0):
+        xmins.append(clamp(float(boxes['x_min']) / width))
+        xmaxs.append(clamp(float(boxes['x_max']) / width))
+        ymins.append(clamp(float(boxes['y_min']) / height))
+        ymaxs.append(clamp(float(boxes['y_max']) / height))
 
-    classes_text.append(state_txt)
-    classes.append(state)
+        classes_text.append(state_txt)
+        classes.append(state)
+
   if (len(classes) > 0):
     tf_example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
@@ -104,11 +225,10 @@ def dict_to_tf_example(data, force_dir=None):
   return None
 
 
-def create_tf_record(output_filename,
+def create_tf_record(writer,
                      yaml_file, forced_dir=None, max_len=None):
   
   images = yaml.load(open(yaml_file, 'rb').read())
-  writer = tf.python_io.TFRecordWriter(output_filename)
   max = len(images)
   if (max_len):
     max = max_len
@@ -119,8 +239,36 @@ def create_tf_record(output_filename,
     else:
       i -= 1
 
-  writer.close()
+def create_udacity_tf_record(writer, file_path, forced_dir=None):
+  curr_file_path = None
+  curr_data = []
+  with open(file_path) as f:
+    content = [x.strip() for x in f.readlines()]
+    for line in content:
+      line = line.replace('"', '')
+      splitlines = line.split()
+
+      if not curr_file_path:
+        curr_file_path = splitlines[0]
+
+      if curr_file_path != splitlines[0]:
+        #got ourselves a new one, process the last!
+        tf_example = udacity_to_tf_example(curr_file_path, curr_data, forced_dir)
+        if (tf_example):
+          writer.write(tf_example.SerializeToString())
+        curr_data = []
+
+      curr_file_path = splitlines[0]
+      curr_data.append(splitlines[1:])
+
+
 
 if __name__ == '__main__':
-  create_tf_record('bosch-train.pb', 'combined_train.yaml')
-  # create_tf_record('bosch-test.pb', 'test.yaml', 'rgb/test/', 500)
+  testwriter = tf.python_io.TFRecordWriter('bosch_test_1class.record')
+  trainwriter = tf.python_io.TFRecordWriter('bosch_train_1class.record')
+  # create_udacity_tf_record(trainwriter, 'udacity/labels_traffic.csv', 'udacity/')
+  create_tf_record(trainwriter, 'combined_train.yaml')
+  create_tf_record(testwriter, 'test.yaml', 'rgb/test/',1000)
+  trainwriter.close()
+  testwriter.close()
+
--- test-bosch.py
+++ test-bosch.py
@@ -18,14 +18,15 @@ import time
 from moviepy.editor import VideoFileClip, ImageSequenceClip
 import cv2
 
-MODEL_NAME='./bosch3'
+MODEL_NAME='./fasterrcnn'
+# MODEL_NAME='./bosch-only-1class'
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = './bosch_label_map.pbtxt'
-
-NUM_CLASSES = 13
+PATH_TO_LABELS = './mscoco_label_map.pbtxt'
+# PATH_TO_LABELS = './bosch_label_map1.pbtxt'
+NUM_CLASSES = 90
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -39,6 +40,52 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
+def get_light_state(light, thresh=200):
+  light = cv2.cvtColor(light, cv2.COLOR_BGR2HLS)
+  h, v, s = cv2.split(light)
+
+  state = ['Red', 'Yellow', 'Green']
+  splith = int(light.shape[0] / len(state))
+  state_out =  np.zeros(3)
+  max_state = -1
+  max_state_val = 0
+  for i in range(len(state)):
+    state_out[i] = len(np.where(s[splith * i : min(splith * (i+1), light.shape[0]),:] > thresh)[0])
+    if (state_out[i] > max_state_val):
+      max_state_val = state_out[i]
+      max_state = i
+
+  if (max_state >= 0):
+    return state[max_state], state_out
+  else:
+    return 'Off', None
+
+
+def extract_traffic_light(image,boxes, scores, classes, class_light=10):
+  scores = np.squeeze(scores)
+  boxes = np.squeeze(boxes)
+  classes = np.squeeze(classes).astype(np.int32)
+  image_to_ret = None
+  highest_score = 0
+  for i in range(boxes.shape[0]):
+    if scores is None or scores[i] > 0.5 and classes[i] == class_light:
+
+      if (scores[i] > highest_score):
+        highest_score = scores[i]
+        box = tuple(boxes[i].tolist())
+        ymin, xmin, ymax, xmax = box
+        startx = int(xmin * image.shape[1])
+        endx = int(xmax * image.shape[1])
+        starty = int(ymin * image.shape[0])
+        endy = int(ymax * image.shape[0])
+        print ('acc score: {} return x: {},{} y: {},{} shape: {}'.format(scores[i],startx,endx,starty,endy,image.shape))
+        image_to_ret = image[starty : endy, startx:endx,:]
+      else:
+        print ('rej score: {} return x: {},{} y: {},{} shape: {}'.format(scores[i],startx,endx,starty,endy,image.shape))
+  if (image_to_ret is None):
+    print ('couldnt find light!')
+  return image_to_ret
+
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
@@ -49,11 +96,14 @@ def load_image_into_numpy_array(image):
 # image2.jpg
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
 # TEST_IMAGE_PATHS = glob.glob('/home/faraz/Code/bosch-to-tfrecord/rgb/test/*.png')
-TEST_IMAGE_PATHS = sorted(glob.glob('/tmp/loop/camera/*.png'))
+# TEST_IMAGE_PATHS = sorted(glob.glob('/tmp/loop/camera/*.png'))
+TEST_IMAGE_PATHS = sorted(glob.glob('/tmp/camera/*.png'))
+# TEST_IMAGE_PATHS = sorted(glob.glob('tl_detected.png'))
 # Size, in inches, of the output images.
-IMAGE_SIZE = (48, 32)
+IMAGE_SIZE = (24, 16)
 frames = []
-i=300
+i=0
+plot = False
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     # Definite input and output Tensors for detection_graph
@@ -61,15 +111,16 @@ with detection_graph.as_default():
     # Each box represents a part of the image where a particular object was detected.
     detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
     # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
+    # Score is shown on the result image, together with the class label.https://console.cloud.google.com/logs/viewer?resource=ml_job%2Fjob_id%2Ffaraz_object_detection_1507318078&project=trainbosch
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
     for image_path in TEST_IMAGE_PATHS[i:]:
       start = time.time()
-      image = Image.open(image_path)
+      # image = Image.open(image_path)
       image_np = cv2.imread(image_path)
       image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+
       # image = image.resize((1200,900))
       # the array based representation of the image will be used later in order to prepare the
       # result image with boxes and labels on it.
@@ -83,6 +134,18 @@ with detection_graph.as_default():
           feed_dict={image_tensor: image_np_expanded})
       diff2 = time.time() - start
       # Visualization of the results of a detection.
+      # class == 10 for coco, 1 for ours
+      light = extract_traffic_light(image_np, np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes),class_light=10)
+      state = 'No Light'
+      if (light is not None):
+        state, values = get_light_state(light)
+      print state
+      cv2.putText(image_np, state, (230, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2, cv2.LINE_AA)
+
+      # print ct.get_palette(color_count=10)
+      # print image.getcolors(5000)
+      # plt.imshow(light)
+      # plt.show()
       vis_util.visualize_boxes_and_labels_on_image_array(
           image_np,
           np.squeeze(boxes),
@@ -91,17 +154,19 @@ with detection_graph.as_default():
           category_index,
           use_normalized_coordinates=True,
           line_thickness=4,
-          min_score_thresh=0.4)
-      # plt.figure(figsize=IMAGE_SIZE)
-      # plt.imshow(image_np)
+          min_score_thresh=0.5)
+      if plot:
+        plt.figure(figsize=IMAGE_SIZE)
+        plt.imshow(image_np)
+        plt.show(2)
       frames.append(image_np)
       diff3 = time.time() - start
       print('Saved {} image, diff1: {} diff2: {}, diff3: {}'.format(i, diff1, diff2, diff3))
       i+=1
       if i > 500:
         break
-      # plt.show()
 
-clip = ImageSequenceClip(frames, fps=20)
+
+clip = ImageSequenceClip(frames, fps=10)
 clip.write_videofile('output.mp4')
 
